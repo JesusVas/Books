7cbfa4f0893fff3aff23791e4e2d661ca022e6b7
Jesus Vasquez <jesusvasquezesc7@gmail.com>
2024-05-05 10:47:29 -0400

    First commit of books

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import BookShow from './BookShow'
+
+const BookList=({books})=>{
+    const renderedBooks=books.map((book)=>{
+        return <BookShow key={book.id} book={book}/>
+    })
+    return(
+        <div className='book-list'>
+            {renderedBooks}
+        </div>
+    );
+}
+return BookList;
